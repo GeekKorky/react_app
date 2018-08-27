f9a3e5e4f4a21a13d15b1663213c6742860e590a
GeekKorky <kaori03252014@gmail.com>
2018-08-27 10:13:13 +0800

    Put functionality to add items into existing projects

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react';
-import Projects from './Components/Projects';
-import AddProject from './Components/AddProject';
-import './App.css';
+import React, { Component } from "react";
+import Projects from "./Components/Projects";
+import AddProject from "./Components/AddProject";
+import "./App.css";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
       projects: []
-    }
+    };
   }
 
-  componentWillMount(){
-    this.setState({projects: [
-    {
-      title: 'Business Website',
-      category: 'Web Design'
-    },
-    {
-      title: 'Social App',
-      category: 'Mobile Development'
-    },
-    {
-      title: 'Ecommerce Shopping Cart',
-      category: 'Web Development'
-    }
-    ]});
+  componentWillMount() {
+    this.setState({
+      projects: [
+        {
+          title: "Business Website",
+          category: "Web Design"
+        },
+        {
+          title: "Social App",
+          category: "Mobile Development"
+        },
+        {
+          title: "Ecommerce Shopping Cart",
+          category: "Web Development"
+        }
+      ]
+    });
+  }
+
+  //add form into state component
+  handleAddProject(project) {
+    //console.log(project);
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({ projects: projects });
   }
 
   render() {
     return (
       <div className="App">
-         <AddProject/>
-         <Projects projects={this.state.projects}/>
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
